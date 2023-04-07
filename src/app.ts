2060f8aa6d4ac5ce6313334dbdea95c2e5fa608b
Gustavo Costa <gustavocosta.cba@gmail.com>
2023-04-07 13:59:04 -0400

    feat cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import Fastify from 'fastify'
 import fastifyJwt from '@fastify/jwt'
-import cookies from '@fastify/cookie'
+import fastifyCookies from '@fastify/cookie'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
+import fastifyCors from '@fastify/cors'
 
 import users from './routes/users'
 import meals from './routes/meals'
@@ -10,7 +13,33 @@ export const app = Fastify({
   logger: false,
 })
 
-app.register(cookies)
+app.register(fastifyCors, {
+  origin: '*',
+})
+app.register(fastifyCookies)
+app.register(fastifySwagger)
+app.register(fastifySwaggerUi, {
+  routePrefix: '/documentation',
+  uiConfig: {
+    docExpansion: 'full',
+    deepLinking: false,
+  },
+  uiHooks: {
+    onRequest: function (request, reply, next) {
+      next()
+    },
+    preHandler: function (request, reply, next) {
+      next()
+    },
+  },
+  staticCSP: true,
+  transformStaticCSP: (header) => header,
+  transformSpecification: (swaggerObject) => {
+    return swaggerObject
+  },
+  transformSpecificationClone: true,
+})
+
 app.register(fastifyJwt, {
   secret: 'supersecret',
 })
--- src/routes/login.ts
+++ src/routes/login.ts
@@ -3,6 +3,9 @@ import { User } from '../interfaces/user-interface'
 import { prisma } from '../database'
 
 export default async function login(app: FastifyInstance) {
+  app.addHook('onRequest', async (request) => {
+    console.log(request.method, request.url)
+  })
   app.post('/', async (request, reply) => {
     try {
       const user = request.body as User
